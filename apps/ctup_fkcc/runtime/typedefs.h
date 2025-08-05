8584fb495b200a91b982975457a4e278d8d1d175
RadhikaG <radhikaghosal@gmail.com>
2025-08-05 01:09:52 -0400

    ctup-int: fast is_any_negative impl, turned on zero prop in ctup, generated panda

--- apps/ctup_fkcc/runtime/typedefs.h
+++ apps/ctup_fkcc/runtime/typedefs.h
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <array>
 #include "blaze/Math.h"
+#include <immintrin.h>
 
 namespace ctup_runtime {
 
@@ -37,6 +38,17 @@ using ConfigurationBlockRobot = std::array<avx256f, Robot::ndim>;
 
 template <size_t ndim>
 using ConfigurationBlockDim = std::array<avx256f, ndim>;
+
+// todo: remove intrinsics and make backend agnostic
+static inline bool is_any_negative(const blaze::StaticVector<float, 8UL>& v) {
+  __m256 vec = _mm256_loadu_ps(v.data());         // Load 8 floats
+  int mask = _mm256_movemask_ps(vec);             // Extract sign bits from all 8 lanes
+  return mask != 0;                               // Any negative if any sign bit set
+}
+
+//static inline bool is_any_negative(const blaze::StaticVector<float, 8UL>& v) {
+//  return blaze::min(v) <= 0;
+//}
 
 }
 
