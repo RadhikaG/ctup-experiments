1469b7e52d15239d1e9cbf4352f52705522733bc
RadhikaG <radhikaghosal@gmail.com>
2025-08-11 06:44:03 +0000

    pin-int: rediscovered cursed march/mavx2 flag issue with pin, added static lib header+impl for vamp-pin compat

---
+++ apps/ctup_fkcc/runtime/pin_compat/pin_collision.h
@@ -0,0 +1,33 @@
+#ifndef CTUP_FKCC_PIN_TYPEDEFS_H
+#define CTUP_FKCC_PIN_TYPEDEFS_H
+#include <memory>
+#include "Eigen/Dense"
+#include <vamp/collision/environment.hh>
+
+namespace ctup_runtime {
+
+namespace pin_compat {
+
+template <typename Robot>
+using ConfigurationBlockRobotEigen = std::array<Eigen::Matrix<float, Robot::ndim, 1>, 8>;
+
+template <size_t ndim>
+using ConfigurationBlockDimEigen = std::array<Eigen::Matrix<float, ndim, 1>, 8>;
+
+struct PinFKCC {
+
+  PinFKCC(std::string robot_name, const vamp::collision::Environment<float> &vamp_env);
+  ~PinFKCC();
+  
+  template <size_t ndim>
+  bool fkcc_pin(const ConfigurationBlockDimEigen<ndim> &q_block);
+
+  struct Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+}
+
+}
+
+#endif
